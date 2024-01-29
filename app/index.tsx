1ec7948e665088acd4935356a9980fa38bc13ff7
Richmond Gakpetor <gakpetorr01@gmail.com>
2024-01-29 09:48:37 +0000

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+
+export default function App() {
+    return (
+
+        <View className='flex flex-1 px-2 items-center justify-center bg-white'>
+            <Text className='text-lg text-blue-500 font-semibold '>Open up App.js to start working on your app!</Text>
+        </View>
+    );
+}
+
